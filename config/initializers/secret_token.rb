1c34893c0fb4c557618f3c822d57bffaa1a8f1b1
Bonnie <so_bonnie@hotmail.com>
2013-12-13 09:46:20 -0800

    Initializing project with status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialapp::Application.config.secret_key_base = '66315ea45bd1737a05be18406a0735be617b4ed0fc6eca0e9d36a69985f7e4a70a043136005b85d9a486a96ad800a17859ade8ffa2c906c713a5b8061369bd36'
